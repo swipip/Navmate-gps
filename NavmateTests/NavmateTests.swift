f1612a92f2320b981c7a2b429e8d9832dc9ba2f8
GautierBillard <56152915+swipip@users.noreply.github.com>
2020-05-11 22:57:19 +0200

    add CRD and navigation

---
+++ NavmateTests/NavmateTests.swift
@@ -0,0 +1,22 @@
+//
+//  NavmateTests.swift
+//  NavmateTests
+//
+//  Created by Gautier Billard on 10/05/2020.
+//  Copyright © 2020 Gautier Billard. All rights reserved.
+//
+
+import XCTest
+//@testable import Navmate
+
+
+
+class NavmateTests: XCTestCase {
+
+//    func testMain() {
+//        var hello: String?
+//
+//        XCTAssertNil(hello)
+//    }
+
+}
